d23bfa419f1f3d959a7a662a716fd9d78663783f
modeco80 <lily.modeco80@protonmail.ch>
2022-05-08 14:59:29 -0500

    *: runtime -> mlstd printf (oops)

--- src/mlstd/crt/printf.cpp
+++ src/mlstd/crt/printf.cpp
@@ -12,24 +12,24 @@
 extern "C" {
 // int vsnprintf(char* __restrict buf, int len, const char* __restrict format, va_list va);
 
-int elfldr_vprintf(const char* __restrict format, va_list vs) {
+int mlstd_vprintf(const char* __restrict format, va_list vs) {
 	static char buffer[2048] {};
 	int res = vsnprintf(buffer, sizeof(buffer), format, vs);
 	sio_putsn(buffer);
 	return res;
 }
 
-int elfldr_printf(const char* __restrict format, ...) {
+int mlstd_printf(const char* __restrict format, ...) {
 	__builtin_va_list val;
 	__builtin_va_start(val, format);
 
-	int res = elfldr_vprintf(format, val);
+	int res = mlstd_vprintf(format, val);
 
 	__builtin_va_end(val);
 	return res;
 }
 
-int elfldr_puts(const char* __restrict string) {
+int mlstd_puts(const char* __restrict string) {
 	return sio_puts(string);
 }
 }
